ef41a73b4b3231859ece7b18fe65b36f4ca6d49b
Kalle <kf101@mail.de>
2024-01-06 19:26:04 +0100

    removed unused functions

--- files/import.js
+++ files/import.js
@@ -1,14 +1,3 @@
-let ab2str = function(buf) {
-    return String.fromCharCode.apply(null, new Uint16Array(buf));
-};
-let str2ab = function(str) {
-    var buf = new ArrayBuffer(str.length*2); // 2 bytes for each char
-    var bufView = new Uint16Array(buf);
-    for (var i=0, strLen=str.length; i<strLen; i++) {
-        bufView[i] = str.charCodeAt(i);
-    }
-    return buf;
-};
 let UTF8_bin = function() {
     ergebnis = "";
     for (let i = 0; i < 512; i++) {
@@ -28,43 +17,6 @@ let UTF8_bin = function() {
     }
 };
 let utf8 = ` !"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[]^_abcdefghijklmnopqrstuvwxyz{|}~ ¡¢£¤¥¦§¨©ª«¬­®¯°±²³´µ¶·¸¹º»¼½¾¿ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõö÷øùúûüýþÿĀāĂăĄąĆćĈĉĊċČčĎďĐđĒēĔĕĖėĘęĚěĜĝĞğĠġĢģĤĥĦħĨĩĪīĬĭĮįİıĲĳĴĵĶķĸĹĺĻļĽľĿŀŁłŃńŅņŇňŉŊŋŌōŎŏŐőŒœŔŕŖŗŘřŚśŜŝŞşŠšŢţŤťŦŧŨũŪūŬŭŮůŰűŲųŴŵŶŷŸŹźŻżŽžſƀƁƂƃƄƅƆƇƈƉƊƋƌƍƎƏƐƑƒƓƔƕƖƗƘƙƚƛƜƝƞƟƠơƢƣƤƥƦƧƨƩƪƫƬƭƮƯưƱƲƳƴƵƶƷƸƹƺƻƼƽƾƿǀǁǂǃǄǅǆǇǈǉǊǋǌǍǎǏǐǑǒǓǔǕǖǗǘǙǚǛǜǝǞǟǠǡǢǣǤǥǦǧǨǩǪǫǬǭǮǯǰǱǲǳǴǵǶǷǸǹǺǻǼǽǾǿ`;
-let encodeImage = function(data) {
-    let number = 0;
-    let character = data[0];
-    let result = "";
-    for (let i = 0; i < data.length; i++) {
-        if (data[i] != "0" && data[i] != "1") throw `Unexpected "${data[i]}": only "0" and "1" allowed`;
-        if (data[i] == character) number++;
-        else {
-            result+= utf8[number];
-            number = 1;
-            character = data[i];
-            continue;
-        }
-        if (number == utf8.length) {
-            result+= utf8[number-1];
-            result+= utf8[0];
-            number = 1;
-        }
-    }
-    result+= utf8[number];
-    return result;
-};
-let decodeImage = function(data) {
-    let string = "";
-    for (let i = 0; i < data.length; i++) {
-        string += (""+i%2).repeat(utf8.indexOf(data[i]));
-    }
-    let array = new Uint32Array(Math.ceil(string.length/32));
-    for (let i = 0; i < array.length; i++) {
-        array[i] = 0;
-        for (let j = 0; j < 32; j++) {
-            if (j) array[i] <<= 1;
-            if (string[i*32+j] == "1") array[i]++;
-        }
-    }
-    return array;
-};
 async function loadImageURL(URL) {
     //create img element and load URL
     let img = document.createElement("img");
